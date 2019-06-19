25532bc22bbc1aa3ad55f19fbbe719bd1f990c39
AdarrFX <grenador@gmail.com>
2019-06-19 12:12:45 -0400

    Responsive styling and menus completed

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
 
 const main = function () {
 
-    console.log("Hello!");
+    let isScreenDimmed = false;
 
     $(window).scroll(updateNav);
 
@@ -27,13 +27,43 @@ const main = function () {
           $('html, body').animate({
             scrollTop: $(hash).offset().top
           }, 800, function(){
-    
             // Add hash (#) to URL when done scrolling (default click behavior)
             // window.location.hash = hash;
           });
+          $(".navbar-responsive").removeClass("navbar-shown");
+          $(".screen-dim").addClass("hidden-element");
+          removeBurgerStyle();
+          $("body").removeClass("screen-dim-active");
         } // End if
       });
 
+      responsiveNav();
+
+}
+
+const responsiveNav = function() {
+    $(".tasty-burger").on("click", function(event){
+        console.log("Yo!");
+
+        toggleBurgerStyle();
+
+        $(".navbar-responsive").toggleClass("navbar-shown");
+        $(".screen-dim").toggleClass("hidden-element");
+        $("body").toggleClass("screen-dim-active");
+
+    });
+}
+
+const removeBurgerStyle = function() {
+     $(".upper-ham").removeClass("upper-ham-active");
+     $(".lower-ham").removeClass("lower-ham-active");
+     $(".middle-ham").removeClass("middle-ham-active");
+}
+
+const toggleBurgerStyle = function() {
+    $(".upper-ham").toggleClass("upper-ham-active");
+    $(".lower-ham").toggleClass("lower-ham-active");
+    $(".middle-ham").toggleClass("middle-ham-active");
 }
 
 const updateNav = function() {
